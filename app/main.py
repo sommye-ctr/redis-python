1e77ff21bebc1c454bdc4e664fbeece6ba2cf33c
Somye <anyone.mahajan@gmail.com>
2025-08-29 01:58:01 +0530

    Added concurrent connections

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
+import concurrent.futures
 import socket  # noqa: F401
+
+
+def send_pong(conn: socket):
+    while True:
+        req = conn.recv(512)
+        if not req:
+            break
+        data = req.decode()
+        if "ping" in data.lower():
+            conn.sendall(b"+PONG\r\n")
 
 
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _ = server_socket.accept()
-
-    while True:
-        req = conn.recv(512)
-        data = req.decode()
-        if "ping" in data.lower():
-            conn.sendall(b"+PONG\r\n")
+    with concurrent.futures.ThreadPoolExecutor() as e:
+        while True:
+            conn, _ = server_socket.accept()
+            e.submit(send_pong, conn)
 
 
 if __name__ == "__main__":

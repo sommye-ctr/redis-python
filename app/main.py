2f33fab1e38af47690dc3a717874b1b61e21d6e8
Somye <anyone.mahajan@gmail.com>
2025-08-28 17:54:10 +0530

    Added handling multiple ping

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ import socket  # noqa: F401
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, _ = server_socket.accept()
-    conn.sendall(b"+PONG\r\n")
+
+    while True:
+        req = conn.recv(512)
+        data = req.decode()
+        if "ping" in data.lower():
+            conn.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":

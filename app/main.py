cebc6cb1fb9d28e4d974e655f79ccac83ca5d41b
Somye <anyone.mahajan@gmail.com>
2025-08-29 12:05:36 +0530

    Added protocol parser for echo and ping

--- app/main.py
+++ app/main.py
@@ -1,23 +1,11 @@
-import concurrent.futures
 import socket  # noqa: F401
 
-
-def send_pong(conn: socket):
-    while True:
-        req = conn.recv(512)
-        if not req:
-            break
-        data = req.decode()
-        if "ping" in data.lower():
-            conn.sendall(b"+PONG\r\n")
+from app.protocol_parser import Protocol
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    with concurrent.futures.ThreadPoolExecutor() as e:
-        while True:
-            conn, _ = server_socket.accept()
-            e.submit(send_pong, conn)
+    protocol = Protocol()
+    protocol.start_listening()
 
 
 if __name__ == "__main__":
---
+++ app/protocol_parser.py
@@ -0,0 +1,59 @@
+import concurrent.futures
+import socket
+
+DOLLAR = '$'
+ASTERISK = '*'
+CRLF = r'\r\n'
+
+
+class Protocol:
+    ECHO_COMMAND = "ECHO"
+    PING_COMMAND = "PING"
+
+    host = 'localhost'
+    port = 6379
+    _server_socket = None
+
+    def __init__(self):
+        self._server_socket = socket.create_server((self.host, self.port), reuse_port=True)
+
+    def start_listening(self):
+        with concurrent.futures.ThreadPoolExecutor() as e:
+            while True:
+                conn, _ = self._server_socket.accept()
+                e.submit(self._get_request_data, conn)
+
+    def _get_request_data(self, connection: socket):
+        while True:
+            print("inside method")
+            req = connection.recv(512)
+            if not req:
+                break
+            data = req.decode()
+            result = self._parse(data)
+            print(result)
+            connection.sendall(result)
+
+    def _parse(self, data: str):
+        lines = data.split(CRLF)
+        if lines[0][0] == ASTERISK:
+            return self._handle_bulk_string(lines[1:], lines[0][1])
+
+    def _handle_bulk_string(self, lines: [str], n: str):
+        try:
+            length = int(n)
+        except ValueError:
+            raise "Bad Request"
+
+        if len(lines) == 0:
+            raise "Bad Request"
+
+        match lines[1]:
+            case self.ECHO_COMMAND:
+                res = f"{DOLLAR}{CRLF}{lines[3]}{CRLF}"
+            case self.PING_COMMAND:
+                res = f"{DOLLAR}{CRLF}+PONG\r\n"
+            case _:
+                res = ""
+
+        return res.encode('utf-8')

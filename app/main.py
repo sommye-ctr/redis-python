b8e26044b9e36f4fd926eaa2c694e9b516f52de1
Somye <anyone.mahajan@gmail.com>
2025-10-10 23:34:26 +0530

    Made replicaof take str as input

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ from app.protocol_parser import Protocol
 async def main():
     parser = ArgumentParser()
     parser.add_argument("--port", type=int, default=6379, help="Set port for server")
-    parser.add_argument("--replicaof", nargs=2, help="Master port and server")
+    parser.add_argument("--replicaof", type=str, help="Master port and server")
     args = parser.parse_args()
 
     if args.replicaof:
-        m_host, m_port = args.replicaof
-        protocol = Protocol(port=args.port, master_port=m_port, master_host=m_host, is_master=False)
+        m_host, m_port = args.replicaof.split()
+        protocol = Protocol(port=args.port, master_port=int(m_port), master_host=m_host, is_master=False)
     else:
         protocol = Protocol(port=args.port)
     await protocol.start_listening()
